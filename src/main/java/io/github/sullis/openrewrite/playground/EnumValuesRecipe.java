11507e8e8817d4932f43b532ab8a28aa6d1d8313
sullis <github@seansullivan.com>
2024-04-23 12:37:11 -0700

    add EnumValuesRecipe.java

---
+++ src/main/java/io/github/sullis/openrewrite/playground/EnumValuesRecipe.java
@@ -0,0 +1,15 @@
+package io.github.sullis.openrewrite.playground;
+
+import org.openrewrite.Recipe;
+
+public class EnumValuesRecipe extends Recipe {
+  @Override
+  public String getDisplayName() {
+    return "Java enum values";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Java enum values";
+  }
+}

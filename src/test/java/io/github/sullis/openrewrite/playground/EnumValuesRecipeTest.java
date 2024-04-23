c277369eb0afa74feeee6a3c62869c5edb6430c9
sullis <github@seansullivan.com>
2024-04-23 12:53:48 -0700

    update EnumValuesRecipeTest

--- src/test/java/io/github/sullis/openrewrite/playground/EnumValuesRecipeTest.java
+++ src/test/java/io/github/sullis/openrewrite/playground/EnumValuesRecipeTest.java
@@ -1,12 +1,21 @@
 package io.github.sullis.openrewrite.playground;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class EnumValuesRecipeTest implements RewriteTest {
+  @Override
+  public void defaults(RecipeSpec spec) {
+    spec
+        .recipe(new EnumValuesRecipe())
+        .parser(JavaParser.fromJavaVersion());
+  }
+
   @Test
   void helloWorld() {
     assertThat(true).isTrue();

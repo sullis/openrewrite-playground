cd2cb578b31ec92b1524bf945868319e7a31a5a9
sullis <github@seansullivan.com>
2024-04-23 12:47:01 -0700

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.2")
+
+    testImplementation("org.openrewrite:rewrite-java-17")
+    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.openrewrite:rewrite-maven")
 }
 
 tasks.test {
